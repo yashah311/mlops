d95e26c72aecc45f297ccc10c992d1cf18bfee05
yash.shah <yashah311@gmail.com>
2026-05-30 10:49:30 +0530

    update name

--- pipelines/deploy_endpoint.py
+++ pipelines/deploy_endpoint.py
@@ -30,7 +30,7 @@ def deploy_or_update_endpoint():
     print(f"Found latest approved model package: {latest_package_arn}")
     
     # 2. Naming Layout Definitions
-    endpoint_name = f"{config['aws']['model_package_group_name']}-prod"
+    endpoint_name = f"{config['aws']['model_package_group_name']}-dev-endpoint"
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     endpoint_config_name = f"{endpoint_name}-config-{timestamp}"
     model_name = f"{config['aws']['model_package_group_name']}-model-{timestamp}"
